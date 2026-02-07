11bccbe7b41332cfdb18464e337be6587af4a5dd
TravidB <travisbu12345@gmail.com>
2026-02-06 23:40:26 -0800

    final commit

--- src/main/java/com/github/tb25/Main.java
+++ src/main/java/com/github/tb25/Main.java
@@ -5,31 +5,40 @@ package com.github.tb25;
 public class Main {
     public static void main(String[] args) {
 
-            int[] sortedArr = {1, 3, 7, 8, 2, 4, 5, 6};
-            Main m = new Main();
-            m.mergeSort(sortedArr);
-            for(int r :  sortedArr){
-                System.out.print(r);
+        int[] sortArr1 = {1, 3, 7};
+        int[] sortArr2 = {2, 4, 5};
+        Main m = new Main();
+        m.mergeArray(sortArr1, sortArr2);
+    }
+
+    public int[] mergeArray(int[] sortArr1, int[] sortArr2) {
+        int[] mergeArr = new int[sortArr1.length + sortArr2.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < sortArr1.length && j < sortArr2.length) {
+            if (sortArr1[i] <= sortArr2[j]) {
+                mergeArr[k] = sortArr1[i++];
+            } else {
+                mergeArr[k] = sortArr2[j++];
             }
-
-    }
-
-    public int[] mergeSort(int[] sortArr) {
-
-        mergeSortHelper(sortArr, 0 , sortArr.length - 1);
-        return sortArr;
-    }
-
-    public void mergeSortHelper(int[] arr, int l, int r) {
-        if(l < r){
-            int m = (l + r) / 2;
-            mergeSortHelper(arr, l, m);
-            mergeSortHelper(arr, m + 1, r);
-
+            k++;
         }
-    }
 
-    public void merge(){
+        while (i < sortArr1.length) {
+            mergeArr[k++] = sortArr1[i++];
+        }
 
+        while (j < sortArr2.length) {
+            mergeArr[k++] = sortArr1[j++];
+        }
+
+        for(int e : mergeArr) {
+            System.out.print(e + " ");
+        }
+
+        return mergeArr;
     }
 }
+
---
+++ src/main/java/com/github/tb25/mergeTest.java
@@ -0,0 +1,35 @@
+package com.github.tb25;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class mergeTest {
+    Main m = new Main();
+
+    @Test
+    void testMerge1() {
+        int [] a = {1, 2, 3};
+        int [] b = {4, 5, 6};
+
+        int[] result = {1, 2, 3 , 4, 5, 6};
+        int[] merge = m.mergeArray(a,b);
+    }
+
+    @Test
+    void testMerge2() {
+        int [] a = {1, 2, 3, 7};
+        int [] b = {4, 4, 4, 4};
+
+        int[] result = {1, 2, 3, 4, 4, 4, 4, 7};
+        int[] merge = m.mergeArray(a,b);
+    }
+
+    @Test
+    void testMerge3() {
+        int [] a = {2, 3, 6, 7};
+        int [] b = {5, 6, 7, 8};
+
+        int[] result = {2, 3, 5, 6, 7, 8};
+        int[] merge = m.mergeArray(a,b);
+    }
+
+}
